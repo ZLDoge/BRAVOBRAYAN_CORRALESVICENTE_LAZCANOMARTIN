c48d714ecde8d1b5af5f625dab2593ac904909e3
martynoke <104163958+martynoke@users.noreply.github.com>
2024-10-23 23:24:54 -0300

    Delete Fase 2/Evidencias Proyecto/Evidencias Sistema proyecto/IQVIA directory

--- Fase 2/Evidencias Proyecto/Evidencias Sistema proyecto/IQVIA/core/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from .models import archivo
-
-class archivoform(ModelForm):
-    class Meta:  # Cambia 'meta' a 'Meta'
-        model= archivo
-        fields = ['email', 'uploaded_file']
