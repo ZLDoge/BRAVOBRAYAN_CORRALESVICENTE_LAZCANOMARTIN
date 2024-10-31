06a3a83ebd0559568a4017016dbf7b99989878cd
martynoke <104163958+martynoke@users.noreply.github.com>
2024-10-30 22:18:52 -0300

    Delete Fase 2/Evidencias Proyecto/Evidencias Sistema proyecto/NLP/intento1/IQVIA directory

--- Fase 2/Evidencias Proyecto/Evidencias Sistema proyecto/NLP/intento1/IQVIA/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from .views import home, form_archivo, prueba, intento
-
-urlpatterns = [
-    path('', home, name="home"),
-    path('form-archivo', form_archivo, name="form_archivo"),
-    path('prueba', prueba, name="prueba"),
-    path('intento', intento, name="intento"),
-]    
